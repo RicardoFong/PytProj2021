51d4e08889a3b924d281e26d7d5e87060955ba73
RicardoFong <ricfoz@hotmail.com>
2021-04-11 19:01:58 +0200

    Update define_core_chain.py

--- define_core_chain.py
+++ define_core_chain.py
@@ -1,3 +1,23 @@
+from statistics import mode # used for getting most comon element in list, for define_core_chain method
+from Bio.PDB import *
+from Bio import pairwise2 # This library is for applying pairwise comparison of sequences
+# from Bio.SubsMat import MatrixInfo as matlist # Used for applying subtitution matrix in sequence comparison
+from Bio.pairwise2 import format_alignment # DEBUGING Used for printing the alignment. DEBUGING purposes
+from math import log # For use in defining the gap function for sequence comparison parameters
+import sys
+import os
+import argparse
+import re
+
+######################################################################################################################################
+
+
+####### I saw this first function is already in main program #################
+
+
+####### define_core_chain is the bit of code needed to complete program ######
+
+
 def chain_getter(pdb_files):
     """ expects file list as object from function get_files, outputs a generator containing a list of all chain occurrences """
 
@@ -22,3 +42,60 @@ def define_core_chain(chain_iterator):
 
         chains.append(element)
     return mode(chains)
+
+
+
+############# grouping files of a same complex in a value list with complex name as key ##############
+
+def complex_division(pdb_files):
+    """ Parses a list object with names of files, returns a dictionary with the complex name as keys and correspondent files as values. or protein name as keys and files as values, depending on file name structure."""
+
+    cmplx_name = []
+    prot_name = []
+    regex = re.compile("(\w+)\.(\w+)\.(\w+)_(\w)_(\w+)\.pdb")
+    names_unique = []
+
+    complexes = dict()
+
+    for file in pdb_files:
+
+        if regex.match(file):
+
+            file_name_cmplx = get_file_prefix(file)
+            cmplx = file_name_cmplx.split(".")[0]
+            cmplx_name.append(cmplx)
+            names_cmplx = set(cmplx_name)
+            names_unique_cmplx = list(names_cmplx)
+            cmplx_size = len(names_unique_cmplx)
+        else:
+
+            file_name_prot = get_file_prefix(file)
+            protein = file_name_prot.split(".")[0]
+            prot_name.append(protein)
+            names_prot = set(prot_name)
+            names_unique_prot = list(names_prot)
+            prot_size = len(names_unique_prot)
+
+    if regex.match(file):
+
+        i = 0
+        while i < cmplx_size:
+
+            complexes[names_unique_cmplx[i]] = []
+
+            i+=1
+
+        for file in pdb_files:
+            prefix = file.split(".")[0]
+            if prefix in file:
+                complexes.setdefault(prefix,[]).append(file)
+
+    else:
+
+        for file in pdb_files:
+            prefix = file.split("_")[0]
+            if prefix in file:
+                complexes.setdefault(prefix,[]).append(file)
+
+
+    return complexes
